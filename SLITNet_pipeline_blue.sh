0f41ec6547d633a7c5ea3e6723d2ae833b55aaa2
LambdaDevBox <jessicaloohw@gmail.com>
2020-03-31 13:29:02 -0400

    update code

--- SLITNet_pipeline_blue.sh
+++ SLITNet_pipeline_blue.sh
@@ -2,20 +2,21 @@
 export CUDA_VISIBLE_DEVICES=1
 
 
-# Training and validation:
+# Main directory:
 MODEL_DIR='Models/Blue_Light'
+
+# Training and validation:
 for K_FOLD in {'K1','K2','K3','K4','K5','K6','K7'}
 do
     python SLITNet_train_blue.py $MODEL_DIR $K_FOLD 1 'coco' 0 100 $K_FOLD 0 1
     python SLITNet_validate_blue.py $MODEL_DIR $K_FOLD 79 99 $K_FOLD
 done
 
-# Testing:
-MODEL_DIR='Trained_Models/Blue_Light'
-python SLITNet_test_blue.py $MODEL_DIR 'K1' 88 'K1'
-python SLITNet_test_blue.py $MODEL_DIR 'K2' 99 'K2'
-python SLITNet_test_blue.py $MODEL_DIR 'K3' 87 'K3'
-python SLITNet_test_blue.py $MODEL_DIR 'K4' 86 'K4'
-python SLITNet_test_blue.py $MODEL_DIR 'K5' 91 'K5'
-python SLITNet_test_blue.py $MODEL_DIR 'K6' 83 'K6'
-python SLITNet_test_blue.py $MODEL_DIR 'K7' 92 'K7'
+# Testing (replace xx with model number):
+python SLITNet_test_blue.py $MODEL_DIR 'K1' xx 'K1'
+python SLITNet_test_blue.py $MODEL_DIR 'K2' xx 'K2'
+python SLITNet_test_blue.py $MODEL_DIR 'K3' xx 'K3'
+python SLITNet_test_blue.py $MODEL_DIR 'K4' xx 'K4'
+python SLITNet_test_blue.py $MODEL_DIR 'K5' xx 'K5'
+python SLITNet_test_blue.py $MODEL_DIR 'K6' xx 'K6'
+python SLITNet_test_blue.py $MODEL_DIR 'K7' xx 'K7'
--- SLITNet_pipeline_white.sh
+++ SLITNet_pipeline_white.sh
@@ -2,20 +2,21 @@
 export CUDA_VISIBLE_DEVICES=1
 
 
-# Training and validation:
+# Main directory:
 MODEL_DIR='Models/White_Light'
+
+# Training and validation:
 for K_FOLD in {'K1','K2','K3','K4','K5','K6','K7'}
 do
     python SLITNet_train_white.py $MODEL_DIR $K_FOLD 1 'coco' 0 100 $K_FOLD 0 1
     python SLITNet_validate_white.py $MODEL_DIR $K_FOLD 79 99 $K_FOLD
 done
 
-# Testing:
-MODEL_DIR='Trained_Models/White_Light'
-python SLITNet_test_white.py $MODEL_DIR 'K1' 92 'K1'
-python SLITNet_test_white.py $MODEL_DIR 'K2' 96 'K2'
-python SLITNet_test_white.py $MODEL_DIR 'K3' 98 'K3'
-python SLITNet_test_white.py $MODEL_DIR 'K4' 93 'K4'
-python SLITNet_test_white.py $MODEL_DIR 'K5' 80 'K5'
-python SLITNet_test_white.py $MODEL_DIR 'K6' 80 'K6'
-python SLITNet_test_white.py $MODEL_DIR 'K7' 97 'K7'
+# Testing (replace xx with model number):
+python SLITNet_test_white.py $MODEL_DIR 'K1' xx 'K1'
+python SLITNet_test_white.py $MODEL_DIR 'K2' xx 'K2'
+python SLITNet_test_white.py $MODEL_DIR 'K3' xx 'K3'
+python SLITNet_test_white.py $MODEL_DIR 'K4' xx 'K4'
+python SLITNet_test_white.py $MODEL_DIR 'K5' xx 'K5'
+python SLITNet_test_white.py $MODEL_DIR 'K6' xx 'K6'
+python SLITNet_test_white.py $MODEL_DIR 'K7' xx 'K7'
