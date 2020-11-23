352cbafcc053182af8e569b29936a619939273a2
jessicaloohw <32312766+jessicaloohw@users.noreply.github.com>
2020-11-23 10:08:49 -0500

    Update SLITNet_pipeline_white.sh

--- SLITNet_pipeline_white.sh
+++ SLITNet_pipeline_white.sh
@@ -22,3 +22,14 @@ python SLITNet_test_white.py $MODEL_DIR 'K4' xx 'K4'
 python SLITNet_test_white.py $MODEL_DIR 'K5' xx 'K5'
 python SLITNet_test_white.py $MODEL_DIR 'K6' xx 'K6'
 python SLITNet_test_white.py $MODEL_DIR 'K7' xx 'K7'
+
+# Testing with trained models (uncomment to use):
+MODEL_DIR='Trained_Models/White_Light'
+
+#python SLITNet_test_white.py $MODEL_DIR 'K1' 92 'K1'
+#python SLITNet_test_white.py $MODEL_DIR 'K2' 96 'K2'
+#python SLITNet_test_white.py $MODEL_DIR 'K3' 98 'K3'
+#python SLITNet_test_white.py $MODEL_DIR 'K4' 93 'K4'
+#python SLITNet_test_white.py $MODEL_DIR 'K5' 80 'K5'
+#python SLITNet_test_white.py $MODEL_DIR 'K6' 80 'K6'
+#python SLITNet_test_White.py $MODEL_DIR 'K7' 97 'K7'
